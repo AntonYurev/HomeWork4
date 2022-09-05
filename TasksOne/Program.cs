6d8459152fdaa95ffbb6fb805b60c837bc770a4b
Anton <altay77@inbox.ru>
2022-09-05 19:50:06 +0700

    Tasks One solution

---
+++ TasksOne/Program.cs
@@ -0,0 +1,7 @@
+﻿// принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int B = Convert.ToInt32(Console.ReadLine());
+double rezalt = Math.Pow(A,B); 
+Console.Write("А в степени В = "+rezalt);
